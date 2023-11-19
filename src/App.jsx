2c0a6a88d6d18056c05f669278ca04d3c0248aa0
IsaienkoViktor <isaenkov1986@gmail.com>
2023-11-19 16:17:31 +0200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from "react-router";
+import { SharedLayout } from "./shared/components/SharedLayout/SharedLayout";
+import HomePage from "./pages/HomePage/HomePage";
+import CommentPage from "./pages/CommentPage/CommentPage";
 
 function App() {
-  
-
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/:id" element={<CommentPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
